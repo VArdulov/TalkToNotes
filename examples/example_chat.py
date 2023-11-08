a6a0ae9c8e2e7e3b438937b16ba008c8e521c392
Victor Ardulov <victor.ardulov@gmail.com>
2023-11-07 17:45:01 -0800

    Got the chat functionality working with GGUF of LLAMA2

--- examples/example_chat.py
+++ examples/example_chat.py
@@ -1,5 +1,5 @@
 # from llama_index.prompts import PromptTemplate
-from llama_index.llms import HuggingFaceLLM
+from llama_index.llms import LlamaCPP
 from llama_index.embeddings import HuggingFaceEmbedding
 from llama_index import (
     ServiceContext,
@@ -17,35 +17,32 @@ system_prompt = """
 
 processed_organized_data = "../processed_data/"
 hf_embed_model_name = "sentence-transformers/all-MiniLM-L6-v2"
-hf_chat_model_name = "meta-llama/Llama-2-7b-chat-hf"
+# hf_chat_model_name =  "meta-llama/Llama-2-7b-chat-hf"  # Too large to run locally
+llama_cpp_model_url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/blob/main/llama-2-7b-chat.Q5_K_M.gguf"
+llama_cpp_model_path = "models/llama-2-7b-chat.Q5_K_M.gguf"
 if __name__ == "__main__":
-    # document_reader = SimpleDirectoryReader(processed_organized_data, recursive=True, filename_as_id=True)
-    # print(f"Loading documents from {processed_organized_data}!")
-    # documents = document_reader.load_data()
-    # print(f"Loaded {len(documents)} documents")  # was averaging about 15 secs
-
-    llm = HuggingFaceLLM(
-        # generate_kwargs={"do_sample"},
-        # system_prompt=system_prompt,
-        # query_wrapper_prompt=query_wrapper_prompt,
-        # tokenizer_name="StabilityAI/stablelm-tuned-alpha-3b",
-        model_name=hf_chat_model_name,
-        # device_map="auto",
-        # stopping_ids=[50278, 50279, 50277, 1, 0],
-        # tokenizer_kwargs={"max_length": 4096},
-    )
-
     embed_model = HuggingFaceEmbedding(model_name=hf_embed_model_name)
+    llm = LlamaCPP(
+        model_path=llama_cpp_model_path,
+        temperature=0,
+        max_new_tokens=256,
+        context_window=3900,
+        model_kwargs={"n_gpu_layers": 0},
+        generate_kwargs={},
+        # messages_to_prompt=None,
+        # completion_to_prompt=None,
+        verbose=True
+    )
     service_context = ServiceContext.from_defaults(
         llm=llm,  # Since we're just creating the vector store, no llm is necessary for interacting/answering now
-        embed_model=embed_model  # Tells llama_index to use our embed model
+        embed_model=embed_model,  # Tells llama_index to use our embed model
     )
-    # set_global_service_context(service_context)
+
     storage_context = StorageContext.from_defaults(persist_dir="example_index")
     index = load_index_from_storage(storage_context=storage_context, service_context=service_context)
 
     query_engine = index.as_query_engine()
-    response = query_engine.query("What is an eigenvalue?")
+    response = query_engine.query("What are eigenvalues and eigenvectors? Please cite the lectures that cover them", )
     print(response.response)
 
 
